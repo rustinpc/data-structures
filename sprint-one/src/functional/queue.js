a90b0942cbd5b4c93cb6ca13af7c09f4651caeef
rustinpc <rustinpc@gmail.com>
2014-10-29 15:48:03 -0700

    function queue completed.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,33 @@
 var makeQueue = function(){
   var someInstance = {};
-
+  var count = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    count += 1;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function(){
+    var keyRange = [];
+    for (var key in storage) {
+      keyRange.push(key);
+    }
+    var location = Math.min.apply(null,keyRange);
+    var itemToBeReturn = storage[location];
+    delete storage[location];
+    return itemToBeReturn;
   };
 
   someInstance.size = function(){
+    var counter = 0;
+    for (var key in storage){
+      counter += 1;
+    };
+    return counter;
   };
 
   return someInstance;
