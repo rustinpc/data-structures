5657f4e47bb7aac36135d3ca9ab911108f4dedbc
rustinpc <rustinpc@gmail.com>
2014-10-30 11:26:34 -0700

    Prototypal Queue Completed.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.begin = 0;
+  someInstance.end = 0;
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.end] = value;
+  this.end += 1;
+
+};
+
+queueMethods.dequeue = function(){
+  var result = this.storage[this.begin];
+  delete this.storage[this.begin];
+  this.begin += 1;
+  return result;
+
+};
+
+queueMethods.size = function(){
+  if (this.end - this.begin < 0) {
+    this.end = this.begin;
+  }
+  return this.end - this.begin;
+};
 
 
