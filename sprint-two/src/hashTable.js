9b9d024d8de25d8c840eae3823fe1aff6ffbda4f
rustinpc <rustinpc@gmail.com>
2014-11-01 14:27:40 -0700

    hash table workingfile 2.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,40 +5,44 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //insert should take a key, value pair and pass the key into the hash fx to generate unique hash index id;
-  //place key/value pair in storage at last position at hash id position
-  var keyValueObj = {k:v};
-  this._each(function(value, index, storageArray) {
-    if (i === index){
-      if (!value) {
-        this._storage.set(i,[keyValueObj]);
-      } else {
-        value.push(keyValueObj);
-      }
-    }
-  });
+  var keyValueObj = {};
+  keyValueObj[k] = v;
+
+  if (!this._storage.get(i)) {
+    this._storage.set(i,[keyValueObj]);
+  } else {
+    var currentValue = this._storage.get(i);
+    currentValue.push(keyValueObj);
+    this._storage.set(i, currentValue);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var bucketArray = this._storage.get(i);
-  for (var index = 0; index < bucketArray.length; index++) {
-    if (bucketArray[index][k]) {
-      return bucketArray[index][k];
+  var currentValue = this._storage.get(i);
+  for (var index = 0; index < currentValue.length; index++) {
+    if (currentValue[index][k]) {
+      return currentValue[index][k];
     }
   }
-  //take key and pass to hash fx to get unique index hash id;
-  //at hash id, retreive value based on key input by iterating over stored key value pairs at hash id position
-
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(value, index, storageArray) {
-    if (index === i) {
-      storageArray[key] = null;
+  var currentValue = this._storage.get(i);
+  for (var index = 0; index < currentValue.length; index ++) {
+    if (currentValue[index][k]) {
+
     }
-  });
+  }
+
+
+
+  // this._storage.each(function(value, index, storageArray) {
+  //   if (index === i) {
+  //     storageArray[index] = null;
+  //   }
+  // });
   // take key passed in, get unique id with hash fx
   // take unique hash id and remove value from storage array based on hash index id;
 };
