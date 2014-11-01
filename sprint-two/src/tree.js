745d9760d452d20193e9b66a187559e49d358d9b
rustinpc <rustinpc@gmail.com>
2014-10-31 19:52:57 -0700

    Linked List refactored and Tree complete.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -18,33 +18,16 @@ treeMethods.addChild = function(value){
 };
 
 treeMethods.contains = function(target){
-  //check if obj's value === target value
+  var result;
   if (this.value === target) {
-  // return true
     return true;
-  } // if obj's value !== target value
-  else {
-    if (this.children === undefined) {
-      return false;
-    }
-    else {
-      for (var i = 0; i < this.children.length; i++) {
-        return this.children[i].contains(target);
-      }
-    }
   }
-  // check if obj has children;
-  //      if obj has children
-  //          check if children's value == target
-  //          return true
-  //
-  //          if children's value !== target;
-  //            check child's child (recursive)
-  //
-  //      if obj doesn't have chidlren
-  //          return false;
-
-
+  else if (this.children !== undefined) {
+    for (var i = 0; i < this.children.length; i++) {
+      result = result || this.children[i].contains(target);
+    }
+    return result || false;
+  }
 };
 
 
