fb2dc7bace26390e55207e749e38dbd57d29fa5c
rustinpc <rustinpc@gmail.com>
2014-10-31 20:45:17 -0700

    set complete.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  //Create a property/value pair the storage object
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  // check if property exists on object
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  //Remove a property/value pair from the storage object
+  delete this._storage[item];
 };
 
 /*
