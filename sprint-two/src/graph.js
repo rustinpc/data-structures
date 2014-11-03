dab60a4d97627bff55e02a0981cb478b491e954e
Rustin Crandall <rustinpc@gmail.com>
2014-11-02 18:44:31 -0800

    Completed Graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -23,18 +23,20 @@ Graph.prototype.contains = function(node){
   //if yes, return true
   //if no, return false
   return this.hasOwnProperty(node);
-
 };
 
 Graph.prototype.removeNode = function(node){
   //check if the graph object contains 'node' as prop.
   //remove node from graph object
   //remove node from connected edges
-  if (this[node]) {
-    delete this[node];
+  for (i = 0; i < this[node].length; i++) {
+    this.removeEdge(node,this[node][i]);
   }
-
-
+  for (var key in this) {
+    if (this[key][0] === undefined) {
+      delete this[key]; 
+    }
+  }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
@@ -42,17 +44,34 @@ Graph.prototype.getEdge = function(fromNode, toNode){
   //check if toNode has edge of from Node;
   //if both true, return true, else false;
   for (var i = 0; i < this[fromNode].length; i++) {
-    return this[fromNode][i] === toNode;
+    if (this[fromNode][i] === toNode) {
+      return true;
+    }
   }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
   //add reciprocal edges to both fromNode and toNode
-
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  //remove reciprocal edges between fromNode and toNodee
+  //remove reciprocal edges between fromNode and toNode
+  for (var i = 0; i < this[fromNode].length; i++)
+  if (this[fromNode][i] === toNode) {
+    this[fromNode].splice(i, 1);
+  }
+  for (var i = 0; i < this[toNode].length; i++)
+  if (this[toNode][i] === fromNode) {
+    this[toNode].splice(i, 1);
+  }
+  for (var key in this) {
+    if (this[key][0] === undefined) {
+      delete this[key]; 
+    }
+  }
 };
 
 var graph = new Graph();
